e4bb92d3521a5f2d8b6cb5245c852efef794cce9
Gabriel Guillon <gguillon@octo.com>
2016-12-31 11:13:41 +0100

    Including getConf (that failed)

--- libkoca.sh
+++ libkoca.sh
@@ -16,6 +16,11 @@ function koca_b2gmk {	# byte to giga, mega, kilo (tera, peta)
 	w=$1
 	[ -z "$w" ] && read w
 	symbols=(o ko Mo Go To Po ) # Eo, Zo and Yo are too big. 'o' is for alignment
+	#pow=$(echo "scale=2;l($w)/l(10.24)/3" |bc -l)
+	#pow=0${pow%%.*}
+	#pw=$(echo "scale=1;$w/(1.024*10^(3*$pow))" | bc -l)
+	#echo $pw${symbols[$pow]}
+	#return
 	for i in $(seq ${#symbols[*]})
 	do
 		values[$i]=$(echo 1024^$i|bc)
@@ -309,12 +314,12 @@ _getConfIsReadable() {
 	# Sauf qu'un fichier est toujours lisible par root, même s'il 
 	# a le mode 0000 ...
 	local flag=1
-	for file in $conf
+	for file in $KOCA_CONF
 	do
 		# At least one is readable
 		[ -r "$file" ] && flag=0
 	done
-	[ $flag -eq 1 ] && echo "[libkoca] No readable conf file provided. Please put one in an env var named 'conf'" >&2 && return 1
+	[ $flag -eq 1 ] && echo "[libkoca] No readable conf file provided. Please put one in an env var named 'KOCA_CONF'" >&2 && return 1
 	[ $flag -eq 0 ] && return 0
 	return 2
 }
@@ -324,7 +329,7 @@ getConf() {
 	echo "$(date -u +%Y%m%d%H%M%SZ) : $(cd $(dirname \"$0\") ; pwd)/$(basename \"$0\") : getConf" >> /var/libkoca/stats
 	getConfValue "$*"
 }
-# Return values from a configuration file passed in $conf variable
+# Return values from a configuration file passed in $KOCA_CONF variable
 # format of conf file : section.key=value
 # Usage : getConfValue <section> <key>
 function getConfValue {
@@ -336,7 +341,7 @@ function getConfValue {
 	local src=__libkoca__ ; [ -e "$src" ] && eval "$(bash "$src" _getConfGetSedOption _getConfIsReadable)"
 	local opt=$(_getConfGetSedOption)
 	_getConfIsReadable || return $?
-	local val="$(grep -Eh "^$1\.$2[[:space:]]*=" $conf 2>/dev/null | sed -${opt}e 's/[^=*]+=\s*//'| tail -1)"
+	local val="$(grep -Eh "^$1\.$2[[:space:]]*=" $KOCA_CONF 2>/dev/null | sed -${opt}e 's/[^=*]+=\s*//'| tail -1)"
 	[ -n "$val" ] && echo "$val" && return 0
 	[ -n "$3" ] && echo "$3" && return 0
 	return 2
@@ -347,7 +352,7 @@ function getConfAllKeys {
 	local src=__libkoca__ ; [ -e "$src" ] && eval "$(bash "$src" _getConfGetSedOption _getConfIsReadable)"
 	local opt=$(_getConfGetSedOption)
 	_getConfIsReadable || return $?
-	local val=$(grep -Eh "^$1\." $conf | sed -e "s/^$1\.\(.*\)\s*=.*/\1/")
+	local val=$(grep -Eh "^$1\." $KOCA_CONF | sed -e "s/^$1\.\(.*\)\s*=.*/\1/")
 	[ -n "$val" ] && echo $val && return 0
 	return 2
 }
@@ -360,11 +365,11 @@ function getConfAllSections {
 	local v
 	if [ -z "$1" ]
     then
-		v=$(grep -Eh "^[^[:space:]#].*\..*[\.\s=]" $conf | sed -e "s/^\(.*\)\..*\s*=.*/\1/" | sort -u | xargs)
+		v=$(grep -Eh "^[^[:space:]#].*\..*[\.\s=]" $KOCA_CONF | sed -e "s/^\(.*\)\..*\s*=.*/\1/" | sort -u | xargs)
     else
 		while [ "$1" != "" ]
 		do
-			v="$v $(grep -Eh "^[^[:space:]#].*\.$1[\.\s=]" $conf | sed -e "s/^\(.*\)\..*\s*=.*/\1/" | sort -u | xargs)"
+			v="$v $(grep -Eh "^[^[:space:]#].*\.$1[\.\s=]" $KOCA_CONF | sed -e "s/^\(.*\)\..*\s*=.*/\1/" | sort -u | xargs)"
 			shift
 		done
     fi
@@ -618,4 +623,4 @@ do
 	shift
 done
 )
-# built on 2016-07-11
+# built on 2016-12-31
