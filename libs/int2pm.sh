efdfeb86eb49ebf8eda04f6ed061300ba4fd8344
Gabriel Guillon <gguillon@octo.com>
2017-05-30 01:02:18 +0200

    Added int2pm

---
+++ libs/int2pm.sh
@@ -0,0 +1,34 @@
+# Return color code in a specified var
+function int2pm { # return +, ++, +++ (or -)
+	local val="$1"
+	local base="$2"
+	local SCALE=3
+	if ! ( [[ $base =~ ^[0-9]+$ ]] && [[ $val =~ ^-?[0-9]+$ ]] )
+	then
+		echo "Params should be integers" >&2
+		return 1
+	fi
+	if [ $base -lt 0 ]
+	then
+		echo "Base should be positive" >&2
+		return 3
+	fi
+	if [ $base -lt $val ]
+	then
+		echo "Base should be greater than value" >&2
+		return 2
+	fi
+	if [ $val -lt 0 ]
+	then
+		sign='-'
+	else
+		sign='+'
+	fi
+	if [ $val -eq 0 ]
+	then
+		echo '~'
+	else
+		# Weird ?
+		printf "%0.s$sign" {1..$(seq 1 $(echo "scale=0;1+$val/($base/$SCALE)*$val/sqrt($val^2)" | bc ))}
+	fi
+}
---
+++ t/int2pm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# $Id: getColor.sh 1164 2013-01-08 10:15:48Z gab $
+source $(cd $(dirname "$0") ; pwd)/bootstrap.sh
+testReturnGoodResults() {
+	assertEquals "10+" "+" "$(int2pm 10 100)"
+	assertEquals "32+" "+" "$(int2pm 32 100)"
+	assertEquals "34++" "++" "$(int2pm 34 100)"
+	assertEquals "75+++" "+++" "$(int2pm 75 100)"
+	assertEquals "-75---" "---" "$(int2pm -75 100)"
+	assertEquals "-34--" "--" "$(int2pm -34 100)"
+	assertEquals "-32-" "-" "$(int2pm -32 100)"
+	assertEquals "-10-" "-" "$(int2pm -10 100)"
+	assertEquals "Zero" "~" "$(int2pm 0 1)"
+}
+testReturnFalseOnBadParams() {
+	assertFalse "Should have returned false" $(int2pm a 10)
+	assertFalse "Should have returned false" $(int2pm 10 a)
+	assertFalse "Base should be greater thant value" $(int2pm 100 10)
+	assertFalse "Base should be >0" $(int2pm 100 -10)
+}
+source $(cd $(dirname "$0") ; pwd)/footer.sh
