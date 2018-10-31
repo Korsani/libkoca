2ba30868ebc264361e6083056e0b57635507182e
Gabriel Guillon <gguillon@octo.com>
2018-10-31 13:15:31 +0100

    Sorry...

--- t/dieIfNotRoot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# $Id: dieIfNotRoot.sh 1128 2012-08-31 15:44:45Z gab $
-source $(cd $(dirname "$0") ; pwd)/bootstrap.sh
--- t/quotemeta.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# $Id: isIp.sh 1153 2012-12-12 15:31:33Z gab $
-source $(cd $(dirname "$0") ; pwd)/bootstrap.sh
-testQm() {
-	assertEquals '\(\)\-\+\*\ \[\]' "$(koca_quotemeta '()-+* []')"
-}
-source $(cd $(dirname "$0") ; pwd)/footer.sh
