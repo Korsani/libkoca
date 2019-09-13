ec0d66ec8d4db7f4f94d166bc1bff54bc6293044
Gabriel Guillon <gguillon@octo.com>
2019-09-13 10:47:48 +0200

    awk replaced by cut

--- libs/load.sh
+++ libs/load.sh
@@ -3,7 +3,7 @@ function koca_load() { # Return true if load is less or equals to specified floa
 	thr=$1
 	if [[ $thr =~ ^-?0?[1-9]*\.?[0-9]*$ ]]
 	then
-		return  $(bc <<< "$(cat /proc/loadavg | awk '{printf "%.2f",$1}') > $1")
+		return  $(bc <<< "$(cat /proc/loadavg | cut -d ' ' -f 1) > $1")
 	else
 		echo "[__libname__] '$1' is not a float or int" >&2
 		return 2
--- t/loadTest.sh
+++ t/loadTest.sh
@@ -9,8 +9,10 @@ testload() {
 	assertFalse "Load is less than 0" "$r"
 	koca_load 0.0 ; r=$?
 	assertFalse "Load less than 0 should always be false" "$r"
-	overload=$(bc <<< "$(cat /proc/loadavg | awk '{print $1}') + 0.02")
+	overload=$(bc <<< "$(cat /proc/loadavg | cut -d ' ' -f 1) + 0.02")
 	koca_load $overload ; r=$?
-	assertTrue "I should be overload" "$r"
+	assertTrue "I should be overloaded" "$r"
+	koca_load .01 ; r=$?
+	assertFalse "Load should accept .xx form" "$r"
 }
 source $(cd $(dirname "$0") ; pwd)/footer.sh
