b0742f0a2f99cf1e8a8ca9ba32266e6c9104ebb3
Gabriel Guillon <gguillon@octo.com>
2019-09-13 11:22:16 +0200

    Cross-plateform, and fix the decimal separator bug

--- libs/load.sh
+++ libs/load.sh
@@ -1,9 +1,19 @@
 # Return true is load is less or equal to value
 function koca_load() { # Return true if load is less or equals to specified float value
 	thr=$1
+	# Shamelessly stolen from: https://bash.cyberciti.biz/monitoring/monitor-unix-linux-system-load/
+	OS="$(uname)"
+	TRUE="1"
+	if [ "$OS" == "FreeBSD" ]; then
+		FTEXT='load averages:'
+	elif [ "$OS" == "Linux" ]; then
+		FTEXT='load average:'
+	fi
 	if [[ $thr =~ ^-?0?[1-9]*\.?[0-9]*$ ]]
 	then
-		return  $(bc <<< "$(cat /proc/loadavg | cut -d ' ' -f 1) > $1")
+		# Uptime is cross-plateform. cat /proc/loadavg is not
+		# LANG=C make '.' the decimal separator
+		return  $(bc <<< "$(LANG=C uptime | awk -F "$FTEXT" '{ print $2 }' | cut -d, -f1) > $1")
 	else
 		echo "[__libname__] '$1' is not a float or int" >&2
 		return 2
--- t/loadTest.sh
+++ t/loadTest.sh
@@ -9,7 +9,7 @@ testload() {
 	assertFalse "Load is less than 0" "$r"
 	koca_load 0.0 ; r=$?
 	assertFalse "Load less than 0 should always be false" "$r"
-	overload=$(bc <<< "$(cat /proc/loadavg | cut -d ' ' -f 1) + 0.02")
+	overload=10
 	koca_load $overload ; r=$?
 	assertTrue "I should be overloaded" "$r"
 	koca_load .01 ; r=$?
