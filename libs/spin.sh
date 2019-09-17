6ecbdfb3715807f5a615bc83c573ef94d6075ee7
Gabriel Guillon <gguillon@octo.com>
2019-09-17 23:44:42 +0200

    You can now choose the spin

--- libs/spin.sh
+++ libs/spin.sh
@@ -1,7 +1,13 @@
 #http://is.gd/qQc5ab
 function koca_spin {	# Display a spinning cursor
-	#koca_spin=(/ - \\ \| / - \\ \| ) 
-	koca_spin=(\| \) \} \> - \< \{ \( \| \( \{ \< - \> \} \) ) 
-	printf "\b"${koca_spin[$koca_spin_pos]} 
-    (( koca_spin_pos=(koca_spin_pos +1)%${#koca_spin[*]} ))
+	declare -A koca_spin
+	koca_spin[0]='/-\|/-\|'
+	koca_spin[1]='|)}>-<{(|({<->})' 
+	koca_spin[2]='O○·○'
+	spin="$1"
+	[ -z "$spin" ] && spin=0
+	[ $spin -gt ${#koca_spin[*]} ] && spin=0
+	#printf "\b"${koca_spin[$koca_spin_pos]} 
+	printf "\b"${koca_spin[$spin]:$koca_spin_pos:1} 
+    (( koca_spin_pos=(koca_spin_pos +1)%${#koca_spin[$spin]} ))
 }
