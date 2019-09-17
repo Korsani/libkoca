4e142044edbe20bd75453fec5ee1a783d1a8081b
Gabriel Guillon <gguillon@octo.com>
2019-09-17 22:58:52 +0200

    New sequence !

--- libs/spin.sh
+++ libs/spin.sh
@@ -1,6 +1,7 @@
 #http://is.gd/qQc5ab
 function koca_spin {	# Display a spinning cursor
-	koca_spin=(/ - \\ \| / - \\ \| ) 
+	#koca_spin=(/ - \\ \| / - \\ \| ) 
+	koca_spin=(\| \) \} \> - \< \{ \( \| \( \{ \< - \> \} \) ) 
 	printf "\b"${koca_spin[$koca_spin_pos]} 
-    (( koca_spin_pos=(koca_spin_pos +1)%8 ))
+    (( koca_spin_pos=(koca_spin_pos +1)%${#koca_spin[*]} ))
 }
