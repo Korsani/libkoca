e94fb11364d3e6c2a475ebb50278a77d89e06b87
Gab <korsani@free.fr>
2020-01-22 10:10:42 +0100

    Update the tests

--- t/progressTest.sh
+++ t/progressTest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source $(cd $(dirname "$0") ; pwd)/bootstrap.sh
 testprogress()  {
-	s=$(koca_progress '0' 'a');[[ $s =~ ^.0%\ \ \ \[_.*\]a ]];assertTrue 1 $?
-	s=$(koca_progress '50' 'a');[[ $s =~ ^.50% ]];assertTrue 2 $?
-	s=$(koca_progress '100' 'a');[[ $s =~ ^.100%\ \[.*#\]a ]];assertTrue 3 $?
+	s=$(koca_progress '0' 'a');[[ $s =~ ^.0%\ \ \ \[_.*\]a ]];echo $a; assertTrue 0 $?
+	s=$(koca_progress '50' 'a');[[ $s =~ ^.50% ]];assertTrue 50 $?
+	s=$(koca_progress '100' 'a');[[ $s =~ ^.100%\ \[.*#\]a ]];assertTrue 100 $?
 	s=$(koca_progress '101' 'a');assertFalse "More than 100%" $?
 	s=$(koca_progress '-1' 'a');assertFalse "Less than 0" $?
 	s=$(koca_progress 'a' 'a');assertFalse "Not a number" $?
