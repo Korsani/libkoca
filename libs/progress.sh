f6e3641e2f3f8d74b45b2ba41251487aeceb1495
Gab <korsani@free.fr>
2020-01-22 10:09:59 +0100

    Add | in the middle

--- libs/progress.sh
+++ libs/progress.sh
@@ -1,6 +1,6 @@
 koca_progress() {    # progress% suffix
 	COLUMNS=$(tput cols)
-	LANG=C          # avoid ./, mistake
+	LANG=C  		        # avoid ./, mistake
 	local progress="$1" ;
 	[[ $progress =~ ^[0-9]+$ ]] || return 1	# should also catch negative numbres
 	[ $progress -gt 100 ] && return 2
@@ -8,8 +8,11 @@ koca_progress() {    # progress% suffix
 	local SPARSE=$(expr 7 + ${#suf})    # 'xxx% '(5) '['(1) ']'(1)
 	local RATIO=$(echo "scale=2;($COLUMNS-$SPARSE)/100"| bc)
 	#printf "\r%-4s [" "$progress%"
-	fill=$(printf '%.0f' $(echo "scale=0;$progress*$RATIO"|bc))
-	empty=$(expr $COLUMNS - $SPARSE - $fill )
+	local fill=$(printf '%.0f' $(echo "scale=0;$progress*$RATIO"|bc))
+	local empty=$(expr $COLUMNS - $SPARSE - $fill )
+	local half=$(expr \( $COLUMNS - $SPARSE \) \/ 2)
 	# On FreeBSD, head complainx when -c 0
-	printf "\r%-4s [%s%s]%s" $progress% "$(head -c $fill < /dev/zero 2>/dev/null | tr '\0' '#')" "$(head -c $empty < /dev/zero 2>/dev/null| tr '\0' '_')" "$suf"
+	s=$(printf "\r%-4s [%s%s]%s" $progress% "$(head -c $fill < /dev/zero 2>/dev/null | tr '\0' '#')" "$(head -c $empty < /dev/zero 2>/dev/null| tr '\0' '_')" "$suf")
+	# Display a | in the middle
+	echo "${s:0:7+$half-1}|${s:7+$half}"
 }
