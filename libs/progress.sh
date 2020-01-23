db681dd6bef8ea16da875a4144a1505ea76c7db4
Gab <korsani@free.fr>
2020-01-23 02:56:26 +0100

    Faster way of doing!

--- libs/progress.sh
+++ libs/progress.sh
@@ -6,20 +6,24 @@ function koca_progress {    # Display a non blocking not piped progress. Usage: 
 	local NSLICES=${3:-2};[[ $NSLICES =~ ^[0-9]+$ ]] || return 1 
 	local SPARSE=$(expr 7 + ${#suf})    # 'xxx% '(5) '['(1) ']'(1)
 	local slice_size=$(expr \( $COLUMNS - $SPARSE \) \/ $NSLICES)
-	local RATIO=$(echo "scale=2;($COLUMNS-$SPARSE)/100"| bc)
-	#printf "\r%-4s [" "$progress%"
-	local fill=$(printf '%.0f' $(echo "scale=0;$progress*$RATIO"|bc))
-	local empty=$(expr $COLUMNS - $SPARSE - $fill )
+	# Do all the math in bc, thus save some forks...
+	declare -a barfullness
+	local barfullness=($(echo "scale=0;f=$progress*($COLUMNS-$SPARSE)/100;e=($COLUMNS-$SPARSE-f);print f,\" \",e"|bc))
 	# On FreeBSD, head complainx when -c 0
-	#s=$(printf "\r%-4s [%s%s]%s" $progress% "$(head -c $fill < /dev/zero 2>/dev/null | tr '\0' '#')" "$(head -c $empty < /dev/zero 2>/dev/null| tr '\0' '_')" "$suf")
-	local bar=$(printf "%s%s" "$(head -c $fill < /dev/zero 2>/dev/null | tr '\0' '#')" "$(head -c $empty < /dev/zero 2>/dev/null| tr '\0' '_')")
-	# Display |
-	local s=$(printf "\r%-4s [" "$progress%")
-	for p in $(seq 0 $(( $NSLICES-2 )) )
+	local bar=$(printf "%s%s" "$(head -c ${barfullness[0]} < /dev/zero 2>/dev/null | tr '\0' '#')" "$(head -c ${barfullness[1]} < /dev/zero 2>/dev/null| tr '\0' '_')")
+	# - How to put a char at a specific pos in a string, as quick as possible? Please avoid sed blabla stuf...
+	# - Hold my beer
+	declare -a magic
+	# Transform a string in array
+	local magic=($(echo $bar | sed -e 's/\(.\)/\1 /g'))
+	# put | at specific pos in the array
+	for marker_pos in $(seq 1 $(( NSLICES-1 )) )
 	do
-		s="$s${bar:$slice_size*$p:$slice_size-1}"
-		s="$s|"
+		magic[$[marker_pos*slice_size]]='|'
 	done
-	s="$s${bar:$(($slice_size * ($NSLICES-1))):$slice_size}]$suf"
+	# Array back in string
+	bar=$(echo ${magic[@]} | tr -d ' ')
+	local s=$(printf "\r%-4s [%s]%s" "$progress%" "$bar" "$suf" )
+	# Guarantee to fill with blanks
 	printf "%-$((COLUMNS+1))s" "$s"
 }
