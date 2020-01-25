76c04fd4e2297bf86f1f1fdadd6b300aaff4eceb
Gab <korsani@free.fr>
2020-01-26 00:42:41 +0100

    use utf8 symbols

--- libs/progress.sh
+++ libs/progress.sh
@@ -5,20 +5,28 @@ function koca_progress {    # Display a non blocking not piped progress. Usage: 
 	local suf="$2" 
 	local NSLICES=${3:-2};[[ $NSLICES =~ ^[0-9]+$ ]] || return 1 
 	# Perl is 3x faster, and much more easier to implement...
-	perl - "$progress" "$suf" "$NSLICES" "$COLUMNS" << 'EOP'
+	perl -CS - "$progress" "$suf" "$NSLICES" "$COLUMNS" << 'EOP'
+	use utf8;
+	#my $chars='░▒▓█';
+	my $chars='▏▎▍▌▋▊▉█';
+	my $nchars=length($chars);
 	my $p=shift;
 	my $s=shift;
 	my $nslices=shift;
 	my $cols=shift;
 	my $sparse=7+length($s);
-	my $scale=($cols-$sparse)/100;
+	my $scale=$nchars*($cols-$sparse)/100;
 	# Build the bar
-	my $bar='#'x($p*$scale);
-	$bar.='_'x((100-$p)*$scale);
+	$p_scaled=($p*$scale)/$nchars;
+	my $bar=substr($chars,$nchars-1,1)x(int($p_scaled)-1);
+	my $arrow=substr($chars,$nchars*($p_scaled-int($p_scaled)-1),1),"\n";
+	$bar.=$arrow;
+	$bar.=' 'x(1+(100-$p)*$scale/$nchars);
 	my $slice_length=length($bar)/$nslices;
 	# Put the | on the bar
-	map {substr($bar,$slice_length*$_,1,'|')} (1..($nslices-1));
+	# But not on filled part
+	map {substr($bar,$slice_length*$_,1,'|')} (1+int($p_scaled/$slice_length)..($nslices-1));
 	# Print
-	printf "\r%-4s [%s]%-".length($s)."s","${p}%",$bar,$s;
+	printf "\r%-4s[%s]%-".length($s)."s","${p}%",$bar,$s;
 EOP
 }
