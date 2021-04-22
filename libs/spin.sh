7a20df3035803a37c0bd5dbb560e740b6f0bc712
Gabriel Guillon <gabriel@guillon.world>
2021-04-22 09:22:29 +0200

    added demo

--- libs/spin.sh
+++ libs/spin.sh
@@ -3,6 +3,7 @@ function koca_spin {	# Display a spinning cursor or scrolling text. Usage: $0 [ 
 	local spin=${1:-0}
 	local dir=${2:-1}
 	local length=${3:-1}
+	local demo
 	declare -A koca_spin ; local koca_spin
 	koca_spin[0]='/-\|/-\|'
 	koca_spin[1]='|>=<|<=>' 
@@ -15,15 +16,20 @@ function koca_spin {	# Display a spinning cursor or scrolling text. Usage: $0 [ 
 	case $spin in
 		[0-9]) ;;
 		 list) for n in ${!koca_spin[*]} ; do echo "$n:${koca_spin[$n]}" ; done ; return ;;
+		 demo) demo="$dir";;
 		    *) koca_spin[0]="$1" ; spin=0 ;;
 	esac
-	if [ $length -gt ${#koca_spin[$spin]} ]
-	then
+	if [ -n "$demo" ] ; then
+		for i in $(seq 1 $(( ${#koca_spin[$dir]} * 2)) ) ; do
+			koca_spin $demo ; sleep 0.5
+		done
+		return
+	fi
+	if [ $length -gt ${#koca_spin[$spin]} ] ; then
 		echo "[__libname__] Length ($length) must not be greater than the string length (${#koca_spin[$spin]})" >&2
 		return 1
 	fi
-	if ! [[ $dir =~ [01$] ]] 
-	then
+	if ! [[ $dir =~ [01$] ]] ; then
 		echo "[__libname__] Direction must be -1, 0 or 1, not $dir" >&2
 		return 1
 	fi
@@ -35,8 +41,7 @@ function koca_spin {	# Display a spinning cursor or scrolling text. Usage: $0 [ 
 	# Build index of position from where to write
 	# Nii... Make first term to 0 when a=1, n when a=-1
 	index=( $(seq $((a*n*(a-1)/2)) $a $((n-a*n*(a-1)/2)) ) )
-	if [ $dir -eq 0 ] 		# Bouncing: generate the same sequence, backward, starting from n-1, till 1 (as sequence start with 0)
-	then
+	if [ $dir -eq 0 ] ; then		# Bouncing: generate the same sequence, backward, starting from n-1, till 1 (as sequence start with 0)
 		index=( ${index[@]} $(seq $((n-1)) -1 1) )
 	fi
 	# Display $length car, starting from somewhere 
