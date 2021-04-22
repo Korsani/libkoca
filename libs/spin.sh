8d2331411a0174ee614b2e2bf5c0692cd2e138a0
Gabriel Guillon <gabriel@guillon.world>
2021-04-22 07:28:24 +0200

    one more spinner

--- libs/spin.sh
+++ libs/spin.sh
@@ -9,7 +9,9 @@ function koca_spin {	# Display a spinning cursor or scrolling text. Usage: $0 [ 
 	koca_spin[2]='O○·○'
 	koca_spin[3]='[{(|)}]'
 	koca_spin[4]='.oOo'
-	koca_spin[5]='⠇⠋⠙⠸⠴⠦'
+	koca_spin[5]='⡆⠇⠋⠙⠸⢰⣠⣄'
+	koca_spin[6]='⡀⠄⠂⠁⠈⠐⠠⢀'
+	koca_spin[7]='⠉⠒⠤⣀⣉⣒⣤⣭⣶⣿⠿⣛⠛⠭⣉⠉⠒⠤⣀ '
 	case $spin in
 		[0-9]) ;;
 		 list) for n in ${!koca_spin[*]} ; do echo "$n:${koca_spin[$n]}" ; done ; return ;;
