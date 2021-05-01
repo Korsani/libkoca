4e22e200049eff35145c5c9d2a5b8e363aba55af
Gabriel Guillon <gabriel@guillon.world>
2021-05-01 23:03:31 +0200

    Bugfix buggy test

--- t/progressTest.sh
+++ t/progressTest.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 source $(cd $(dirname "$0") ; pwd)/bootstrap.sh
-testprogress()  {
-	COLS=$(tput cols)
-	s=$(koca_progress '0' 'a');[[ $s =~ ^.0%\ \ \ \[_.*\]a ]];echo $a; assertTrue 0 $?
-	s=$(koca_progress '50' 'a');[[ $s =~ ^.50% ]];assertTrue 50 $?
+testProgress()  {
+	COLS="$(tput cols)"
+	COLUMNS=34
+	s=$(koca_progress '0' 'a' | tr -d '\015\033[K\012' ) ; [[ $s =~ ^0%\ +\/\ +\]a ]] ; assertTrue 0 $?
+	s=$(koca_progress '50' 'a') ; [[ $s =~ ^.50% ]];assertTrue 50 $?
 	# Sorry
-	s=$(koca_progress '100' 'a'| tr -d '\015\033[K\012\210\226\342'); [[ $s =~ ^100%\ /\]a ]];assertTrue 100 $?
+	s=$(koca_progress '100' 'a' | tr -d '\015\033[K\012\210\226\342' ); [ "$s" == "100% /]a" ] ; assertTrue 100 $?
 	s=$(koca_progress '101' 'a');assertFalse "More than 100%" $?
 	s=$(koca_progress '-1' 'a');assertFalse "Less than 0" $?
 	s=$(koca_progress 'a' 'a');assertFalse "Not a number" $?
